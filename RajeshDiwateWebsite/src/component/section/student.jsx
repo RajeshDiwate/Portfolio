ffd0e86bded6105049690713b073e9fcc040e1db
Your Name <your.email@example.com>
2025-08-27 14:40:08 +0530

    Changes resume

--- RajeshDiwateWebsite/src/component/section/student.jsx
+++ RajeshDiwateWebsite/src/component/section/student.jsx
@@ -19,7 +19,7 @@ const studentList = [
     imgAlt: "student rajibraj91 rajibraj",
     name: "Primal Infosys",
     degi: "Pune , India",
-    durasion: "(Nov 2021 - Jun 2023)",
+    durasion: "(Mar 2022 - Jun 2023)",
     href: "https://www.primalinfosys.com/",
     desc: "Gather and anayze requiements from team leads and project stakeholders.Develop responsive user interfaces using HTML, CSS, Bootstrap, and React.js.Design and implement features for online event management and hotel booking    applications using React.js, Redux, Material Design, Express.js, Node.js, HTML, and CSS. Write clean, efficient, and cross-browser compatible code.Fix bugs and implement enhancements to improve application functionality and performance.Integrate code using Git for version control and collaboration.Collaborate with cross-functional teams including designers, frontend developers, and testers to deliver high-quality software solutions.Ensure code quality and adherence to coding standards through code reviews and testing. Stay updated with the latest technologies and industry trends to continuously improve development processes and practices",
   },
